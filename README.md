818bd8151703198d94eca8cf29e3da31b40b0c9d
Trace Boggs <Trace.Boggs05@gmail.com>
2024-06-27 22:07:08 -0500

    changed some stuff :)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Portfolio.github.io
+# TBoggs05.github.io
+
+-----6/27/24-----
+
+-Hello! I'm not really a web design guy, so I kept the design simple
+
+-static site implementing html, css, and a dash of JS.
+
+-not much to really say here :)
---
+++ portfolioSite-main/javascript/gridButtons.js
@@ -0,0 +1,23 @@
+//queryselectorAll queries document for all attributes w/ passed selector (class gridButton)
+    var buttons = document.querySelectorAll(".gridButton");
+  //javascript is weakly typed so our var is actually containing a list of all buttons...
+  //we loop thu each button in buttons and for each button in buttons we call a function which we pass that button
+   buttons.forEach(function(button) {
+        //when that button is clicked we call this JS function...
+       button.onclick = function() {
+          //check against filename so that we cant just spam load the same page.
+           var fileName = location.href.split("/").slice(-1)[0];
+           //branches for loading each page.
+           if (button.classList.contains("experienceButton") && fileName != "ExperiencePage.html") {
+               location.href = "../html/portfolioSite-main/html/ExperiencePage.html";
+           } else if (button.classList.contains("educationButton")&& fileName != "EducationPage.html") {
+               location.href = "../html/portfolioSite-main/html/EducationPage.html";
+           } else if (button.classList.contains("projectsButton")&& fileName != "ProjectsPage.html") {
+               location.href = "../html/portfolioSite-main/html/ProjectsPage.html";
+           } else if (button.classList.contains("resumeButton")&& fileName != "ResumePage.html") {
+               location.href = "../html/ResumePage.html";
+           } else if (button.classList.contains("nameButton")&& fileName != "index.html") {
+               location.href = "../../index.html";
+           }
+       };
+   });
